ba9d6eb5d16ae02cb78a40e547e1372580e18a0e
furkan-guvenc <furkan.gvenc@gmail.com>
2024-12-31 00:53:44 +0000

    fix: stylesheet in head warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,10 +31,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
         <body className="next-light-theme">
-          <Main />
-          <NextScript />
+        <Main />
+        <NextScript />
         </body>
       </Html>
     );
